cf6a1a4b0d23d113bc143482dcff4d2c91b023e1
Abdelrahman Tarek Mahmoud <85140058+AbdelrahmanTarekMahmoud@users.noreply.github.com>
2023-05-05 21:17:03 +0300

    Create RawStringLiteral.cpp

---
+++ RawStringLiteral.cpp
@@ -0,0 +1,29 @@
+// C++ program to demonstrate working of raw string literal
+#include <iostream>
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+    // A Normal string
+    string string1 = "Geeks.\nFor.\nGeeks.\n";
+ 
+    // A Raw string
+    string string2 = R"(Geeks.\nFor.\nGeeks.\n)";
+ 
+    cout << string1 << endl;
+ 
+    cout << string2 << endl;
+ 
+    return 0;
+}
+
+#Output
+
+#String1
+#Geeks.
+#For.
+#Geeks.
+
+#String2
+#Geeks.\nFor.\nGeeks.\n
